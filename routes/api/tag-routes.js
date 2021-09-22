9f1cb6ff84f1421f19570364d8453d3e94c2a896
Chris Tolnai <christolnai@gmail.com>
2021-09-22 15:49:59 -0700

    finished product-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,6 +6,12 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    Tag.findAll({include: [{ model: Product, through: ProductTag, as: 'tag_products' }]}).then(data => {res.status(200).json(data)})
+    ;
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
