0a50307cd8498bf0420dd279e293afdab51faa4b
Chris Tolnai <christolnai@gmail.com>
2021-09-23 16:10:22 -0700

    delete route for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -48,6 +48,19 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: { id: req.params.id }
+    }).then(data => {
+
+      if (!data) {
+        res.status(404).json({ message: 'No categories with this id!' });
+        return;
+      }
+      res.status(200).json(data);
+    })
+    .catch (err => {
+      res.status(500).json(err);
+    })
 });
 
 module.exports = router;
